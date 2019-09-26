f4c93762edb642d18295ebd355013c7d2a9c50f7
goodray0379 <33683314+goodray0379@users.noreply.github.com>
2019-09-26 20:47:06 +0900

    생각하기
    문제를 그대로 코드화 시켰습니다.
    인자로 넘겨받은  정수 x에 대해서 각 자릿 수 한자리 숫자로 분리한다음 모두 합(sum)을 합니다.
    x가 이 합(sum)에 나누어 떨어지면 true 아니면 false를 반환합니다.

--- 파트1.자주_나오는_유형/1-1.하샤드 수/Solution.java
+++ 파트1.자주_나오는_유형/1-1.하샤드 수/Solution.java
@@ -1,5 +1,10 @@
-public class Solution {
-    public boolean solution(int x) {
-        return false;
-    }
+class Solution {
+  public boolean solution(int x) {
+      char[] nums = Integer.toString(x).toCharArray();
+      int sum=0;
+      for(int i=0; i<nums.length; i++)
+          sum += nums[i] - '0';
+      if(x%sum==0) return true;
+      else return false;
+  }
 }
