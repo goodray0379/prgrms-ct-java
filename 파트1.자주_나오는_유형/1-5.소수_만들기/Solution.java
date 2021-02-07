92294de50c7156e216835dfa06c8073a1d57c6b2
heoyoungmin <goodray0379@gmail.com>
2021-02-07 17:41:33 +0900

    1-5 소수만들기

--- 파트1.자주_나오는_유형/1-5.소수_만들기/Solution.java
+++ 파트1.자주_나오는_유형/1-5.소수_만들기/Solution.java
@@ -1,5 +1,31 @@
-public class Solution {
+//1-5 소수만들기
+//https://programmers.co.kr/learn/courses/30/lessons/12977
+
+import java.lang.Math;
+
+class Solution {
     public int solution(int[] nums) {
-        return 0;
+        int answer = 0;
+        int sum = 0;
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                for(int k=j+1; k<nums.length; k++){
+                    sum = nums[i] + nums[j] + nums[k];
+                    if(isPrime( sum ))
+                        answer++;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public boolean isPrime(int number){
+        //Math.sqrt(n) => n의 제곱근
+        for(int i=2; i<=Math.sqrt(number); i++){
+            if(number%i==0){
+                return false;
+            }
+        }
+        return true;
     }
 }
