c37dabad8addfe0eb276f0773e9284c7e9276e9a
heoyoungmin <goodray0379@gmail.com>
2021-02-07 16:23:39 +0900

    1-4 영어 끝말잇기 풀이

--- 파트1.자주_나오는_유형/1-4.영어_끝말잇기/Solution.java
+++ 파트1.자주_나오는_유형/1-4.영어_끝말잇기/Solution.java
@@ -1,5 +1,25 @@
-public class Solution {
+//1-4 영어 끝말잇기
+//https://programmers.co.kr/learn/courses/30/lessons/12981
+
+import java.util.*;
+
+class Solution {
     public int[] solution(int n, String[] words) {
-        return new int[] {};
+        int[] answer = {0,0};
+        Set<String> set = new HashSet<String>(); //중복 단어 검색용
+        set.add(words[0]);
+
+        for(int i=1; i<words.length; i++){
+            //중복단어 || 앞단어의 끝문자와 현재단어의 앞문자 비교
+            if( set.contains(words[i]) ||
+                    words[i-1].charAt( words[i-1].length()-1 ) != words[i].charAt(0) ){
+                answer[0] = i%n + 1;
+                answer[1] = i/n + 1;
+                break;
+            }
+            set.add(words[i]);
+        }
+
+        return answer;
     }
 }
