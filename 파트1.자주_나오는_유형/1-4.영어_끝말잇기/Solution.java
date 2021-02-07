6fb05d84a9fda24b4f7aafa8df272f11db250778
heoyoungmin <goodray0379@gmail.com>
2021-02-07 16:45:08 +0900

    1-4 영어 끝말잇기 수정(contains -> add)

--- 파트1.자주_나오는_유형/1-4.영어_끝말잇기/Solution.java
+++ 파트1.자주_나오는_유형/1-4.영어_끝말잇기/Solution.java
@@ -11,13 +11,12 @@ class Solution {
 
         for(int i=1; i<words.length; i++){
             //중복단어 || 앞단어의 끝문자와 현재단어의 앞문자 비교
-            if( set.contains(words[i]) ||
+            if( !set.add(words[i]) || //중복된 값은 add()가 false를 반환
                     words[i-1].charAt( words[i-1].length()-1 ) != words[i].charAt(0) ){
                 answer[0] = i%n + 1;
                 answer[1] = i/n + 1;
                 break;
             }
-            set.add(words[i]);
         }
 
         return answer;
