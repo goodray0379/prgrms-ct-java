6177c88d4f80052ba2ef14f7a52d91a89784ae02
heoyoungmin <goodray0379@gmail.com>
2021-02-08 23:32:55 +0900

    1-6 다음 큰 숫자

--- 파트1.자주_나오는_유형/1-6.다음_큰_숫자/Solution.java
+++ 파트1.자주_나오는_유형/1-6.다음_큰_숫자/Solution.java
@@ -1,5 +1,25 @@
-public class Solution {
+//1-6 다음 큰 숫자
+//https://programmers.co.kr/learn/courses/30/lessons/12911
+
+
+class Solution {
     public int solution(int n) {
-        return 0;
+        int count = getCount(n);
+        while(true){
+            n++;
+            if(getCount(n)==count)
+                break;
+        }
+        return n;
+    }
+
+    public int getCount(int n){
+        String binary = Integer.toBinaryString(n);
+        int count =0;
+        for(int i=0; i<binary.length(); i++){
+            if(binary.charAt(i)=='1')
+                count++;
+        }
+        return count;
     }
 }
