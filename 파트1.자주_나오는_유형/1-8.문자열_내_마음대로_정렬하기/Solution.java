333c2910954abb783c58bd445cf11b798ef5b415
heoyoungmin <goodray0379@gmail.com>
2021-02-15 22:36:38 +0900

    1-8.문자열_내_마음대로_정렬하기

--- 파트1.자주_나오는_유형/1-8.문자열_내_마음대로_정렬하기/Solution.java
+++ 파트1.자주_나오는_유형/1-8.문자열_내_마음대로_정렬하기/Solution.java
@@ -1,5 +1,17 @@
-public class Solution {
+///1-8.문자열_내_마음대로_정렬하기
+//https://programmers.co.kr/learn/courses/30/lessons/12915
+import java.util.*;
+
+class Solution {
     public String[] solution(String[] strings, int n) {
-        return new String[] {};
+        Arrays.sort(strings, (o1, o2) ->{
+            if(o1.charAt(n)>o2.charAt(n))
+                return 1;
+            else if(o1.charAt(n)==o2.charAt(n))
+                return o1.compareTo(o2);
+            else
+                return -1;
+        });
+        return strings;
     }
 }
