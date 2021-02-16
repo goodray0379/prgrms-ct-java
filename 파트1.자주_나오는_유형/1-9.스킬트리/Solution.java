48df40ab030dcfc3b4aed5d4989002d6b78a4ff7
heoyoungmin <goodray0379@gmail.com>
2021-02-17 00:25:54 +0900

    1-9.스킬트리

--- 파트1.자주_나오는_유형/1-9.스킬트리/Solution.java
+++ 파트1.자주_나오는_유형/1-9.스킬트리/Solution.java
@@ -1,5 +1,20 @@
-public class Solution {
+//1-9.스킬트리
+//https://programmers.co.kr/learn/courses/30/lessons/49993
+import java.util.*;
+class Solution {
     public int solution(String skill, String[] skill_trees) {
-        return 0;
+        int answer =0;
+
+        for(int i=0; i<skill_trees.length; i++){
+            StringBuffer sb = new StringBuffer();
+            for(int j=0; j<skill_trees[i].length(); j++){
+                if(skill.contains(skill_trees[i].charAt(j)+""))
+                    sb.append(skill_trees[i].charAt(j)+"");
+            }
+
+            if(skill.startsWith(sb.toString()))
+                answer++;
+        }
+        return answer;
     }
 }
