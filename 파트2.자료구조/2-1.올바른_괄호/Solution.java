bca4fe67d0740a43f679c5c241daef5993a7e526
heoyoungmin <goodray0379@gmail.com>
2021-02-17 22:32:12 +0900

    2-1.올바른_괄호

--- 파트2.자료구조/2-1.올바른_괄호/Solution.java
+++ 파트2.자료구조/2-1.올바른_괄호/Solution.java
@@ -1,5 +1,21 @@
-public class Solution {
-    public boolean solution(String s) {
-        return false;
+//2-1.올바른_괄호
+//https://programmers.co.kr/learn/courses/30/lessons/12909
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(!stack.isEmpty()){
+                if(c==')') stack.pop();
+                else stack.push('(');
+            }else{
+                if(c==')') return false;
+                else stack.push('(');
+            }
+        }
+
+        return stack.isEmpty();
     }
 }
