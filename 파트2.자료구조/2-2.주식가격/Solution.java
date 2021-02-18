5956f5326bc884eda05d515e5a04acc65e5f0715
heoyoungmin <goodray0379@gmail.com>
2021-02-19 00:19:22 +0900

    //2-2.주식가격 수정

--- 파트2.자료구조/2-2.주식가격/Solution.java
+++ 파트2.자료구조/2-2.주식가격/Solution.java
@@ -5,12 +5,9 @@ class Solution {
         int[] answer = new int[prices.length];
         for(int i=0; i<prices.length; i++){
             for(int j=i+1; j<prices.length; j++){
-                if(prices[i]>prices[j]){
-                    answer[i] = j-i;
+                answer[i]++;
+                if(prices[i]>prices[j])
                     break;
-                }
-
-                if(j==prices.length-1) answer[i] = j-i;
             }
         }
         return answer;
