dfef452b39c2aa0ca60e3077dd5a5bc9743f416c
heoyoungmin <goodray0379@gmail.com>
2021-03-02 22:07:30 +0900

    2-7.전화번호_목록

--- 파트2.자료구조/2-7.전화번호_목록/Solution.java
+++ 파트2.자료구조/2-7.전화번호_목록/Solution.java
@@ -1,5 +1,19 @@
-public class Solution {
+//2-7.전화번호_목록
+//https://programmers.co.kr/learn/courses/30/lessons/42577
+import java.util.*;
+
+class Solution {
     public boolean solution(String[] phone_book) {
-        return false;
+        Set<String> set = new HashSet<>();
+        Arrays.sort(phone_book,
+                (o1, o2) -> {return o1.length()-o2.length();});
+        for(String number : phone_book){
+            for(int i=1; i<=number.length(); i++){
+                if(set.contains(number.substring(0, i)))
+                    return false;
+            }
+            set.add(number);
+        }
+        return true;
     }
 }
