1514e7fcf86a51b5116514df41b57197dcbd303e
heoyoungmin <goodray0379@gmail.com>
2021-03-02 21:43:10 +0900

    2-6.위장

--- 파트2.자료구조/2-6.위장/Solution.java
+++ 파트2.자료구조/2-6.위장/Solution.java
@@ -1,5 +1,20 @@
-public class Solution {
+//2-6.위장
+//https://programmers.co.kr/learn/courses/30/lessons/42578
+import java.util.*;
+
+class Solution {
     public int solution(String[][] clothes) {
-        return 0;
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0; i<clothes.length; i++){
+            if(map.containsKey(clothes[i][1])){
+                map.put(clothes[i][1], map.get(clothes[i][1])+1);
+            }else
+                map.put(clothes[i][1], 1);
+        }
+        for(Integer count : map.values()){
+            answer *= (count+1);
+        }
+        return answer-1;
     }
 }
