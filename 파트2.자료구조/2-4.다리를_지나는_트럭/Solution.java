8cbb174bc54f7b04c1a91b0ad38618b45d0df548
heoyoungmin <goodray0379@gmail.com>
2021-03-02 23:19:31 +0900

    2-4.다리를_지나는_트럭

--- 파트2.자료구조/2-4.다리를_지나는_트럭/Solution.java
+++ 파트2.자료구조/2-4.다리를_지나는_트럭/Solution.java
@@ -1,5 +1,43 @@
-public class Solution {
+//2-4.다리를_지나는_트럭
+//https://programmers.co.kr/learn/courses/30/lessons/42583
+import java.util.*;
+class Truck {
+    public int time;
+    public int weight;
+
+    Truck(int time, int weigth){
+        this.time = time;
+        this.weight = weigth;
+    }
+}
+
+class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        return 0;
+        Queue<Integer> wait = new LinkedList<>();
+        Queue<Truck> ing = new LinkedList<>();
+
+        for(int i=0; i<truck_weights.length; i++)
+            wait.offer(truck_weights[i]);
+
+        int time = 0;
+        int sum = 0;
+        while(!wait.isEmpty()||!ing.isEmpty()){
+
+            time++;
+            if(!ing.isEmpty()){
+                if(time - ing.peek().time == bridge_length)
+                    sum -= ing.poll().weight;
+
+            }
+
+            if(!wait.isEmpty()){
+                if(weight>=sum+wait.peek()){
+                    sum += wait.peek();
+                    ing.offer(new Truck(time, wait.poll()));
+                }
+            }
+        }
+
+        return time;
     }
 }
